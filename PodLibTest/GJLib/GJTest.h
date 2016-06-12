ae5588479d0d4d2b044b84558bdc55d32e8dc1b8
GaoJunLostInCode <1117gaojun@163.com>
2016-06-12 15:52:21 +0800

    init

---
+++ PodLibTest/GJLib/GJTest.h
@@ -0,0 +1,15 @@
+//
+//  GJTest.h
+//  PodLibTest
+//
+//  Created by 高 军 on 16/6/12.
+//  Copyright © 2016年 Ankai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GJTest : NSObject
+
+- (void)test;
+
+@end
---
+++ PodLibTest/GJLib/GJTest.m
@@ -0,0 +1,18 @@
+//
+//  GJTest.m
+//  PodLibTest
+//
+//  Created by 高 军 on 16/6/12.
+//  Copyright © 2016年 Ankai. All rights reserved.
+//
+
+#import "GJTest.h"
+
+@implementation GJTest
+
+- (void)test
+{
+    NSLog(@"Haha ...");
+}
+
+@end
